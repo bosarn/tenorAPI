27ea273e988ea7d7ea6f2a276e6383700d1a6783
bosarn <58468105+bosarn@users.noreply.github.com>
2020-04-08 12:00:00 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
+import axios from 'axios';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Results from './components/results';
+import Form from './components/form'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export default class Movies extends React.Component
+{
+  constructor(props){
+    super(props);
+    this.state ={
+      gif: {
+        error: false,
+        data: []
+    }
+    }
+  }
+
+
+  getGif = str => {
+
+      axios.get(`https://api.tenor.com/v1/search?q="${str}&key=F8Y7Y8RTXWM8`)
+     
+      .then(response=>{
+        
+        this.setState({
+          gif: {
+            ...this.state.gif,
+            data: [...response.data.results]
+        }
+        });
+     
+      })
+      .catch(error=>console.log(error))
+
+      
+    
+
+  }
+  render() {
+    return (
+        <div className='App'>
+
+          <h1> Gifpicker</h1>
+          <Form getGif={this.getGif}/>
+          <Results results={this.state.gif.data}/>
+          
+        </div>
+          
+    )
+  }
 }
 
-export default App;
+
